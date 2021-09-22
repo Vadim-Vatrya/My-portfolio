bf3d844cfdc2abaef11e0d9d40ad7e26f1525696
Vadim-Vatrya <VatryaVV@gmail.com>
2021-09-22 23:26:22 +0300

    appBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Switch, Router } from "react-router-dom";
+// import { Switch, Router } from "react-router-dom";
 // import "./App.css";
 
 import Container from "./components/Container";
-import AppBar from "";
+import AppBar from "./components/AppBar";
 
 function App() {
   return (
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,13 @@
 import Navigation from "../Navigation/Navigation";
+import laptop from "../../images/laptop.svg";
 import styles from "./AppBar.module.scss";
 
 const AppBar = () => {
   return (
     <header className={styles.header}>
+      <a href="/" arial-label="logoLaptop">
+        <img src={laptop} alt="logoLaptop" className={styles.logo} />
+      </a>
       <Navigation />
     </header>
   );
