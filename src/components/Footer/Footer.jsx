18d0b041840395885fbaf3b679d2c99ac14ff446
Vadim-Vatrya <VatryaVV@gmail.com>
2021-10-27 00:29:20 +0300

    add styles footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
   return (
     <>
-      <footer>
-        <span> &#169; 2021 | All Rights Reserved | Developed by </span>
-        <p> Vadim Vatrya </p>
+      <footer className={styles.Footer}>
+        <span className={styles.copy}>
+          &#169; 2021 | All Rights Reserved | Developed by
+        </span>
+        <p className={styles.name}> Vadim Vatrya </p>
       </footer>
     </>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 // import {} from "";
 
-const Navigation = () => (
-  <nav>
-    <Link to="/"> HomePage </Link>
-    <Link to="/projects"> My projects </Link>
-    <Link to="/resume"> My resume </Link>
-  </nav>
-);
+const Navigation = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  const menuToggle = () => {
+    setIsOpenMenu((prev) => !prev);
+  };
+  return (
+    <>
+      <nav>
+        <Link to="/"> HomePage </Link>
+        <Link to="/projects"> My projects </Link>
+        <Link to="/resume"> My resume </Link>
+      </nav>
+    </>
+  );
+};
 
 export default Navigation;
